12063688cb589cc2919d9faf1cec596d4fa8ffa1
ChuckTerry <Chuck.Terry@Outlook.com>
2023-07-17 12:49:33 -0400

    Shorten UUID, lowercase alpha as default

--- src/uuidGenerator.js
+++ src/uuidGenerator.js
@@ -1,5 +1,7 @@
-function cipher_Numeric2Alpha(numberOrString, preserveNonNumeric = false) {
-  const characterCipher = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+function cipher_Numeric2Alpha(numberOrString, preserveNonNumeric = false, lowercase = true) {
+  const characterCipher = lowercase
+    ? ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   return [...numberOrString.toString()].reduce((accumulator, character) => {
     return accumulator + (characterCipher[character] || (preserveNonNumeric ? character : ''));
   }, '');
