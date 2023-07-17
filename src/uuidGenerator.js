67651ec3dc7f33724a9fa25f6b0a27685e1362ae
ChuckTerry <Chuck.Terry@Outlook.com>
2023-07-17 04:14:18 -0400

    primitive uuid generation

---
+++ src/uuidGenerator.js
@@ -0,0 +1,13 @@
+function cipher_Numeric2Alpha(numberOrString, preserveNonNumeric = false) {
+  const characterCipher = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  return [...numberOrString.toString()].reduce((accumulator, character) => {
+    return accumulator + (characterCipher[character] || (preserveNonNumeric ? character : ''));
+  }, '');
+}
+
+function makeUuid() {
+  const epoch = Date.now();
+  const random = Math.random().toString().slice(2, 12)
+  return 'uuid-' + epoch + '-' + cipher_Numeric2Alpha(random).toLowerCase();
+}
+
