80f961cd985f7d5871c0fd3855c84a38dae1f6c6
ChuckTerry <Chuck.Terry@Outlook.com>
2023-07-24 06:52:23 -0400

    DRY

--- build/build.js
+++ build/build.js
@@ -1,96 +1,65 @@
 import { readFile, writeFile } from 'node:fs/promises';
 import { Buffer } from 'node:buffer';
 import { mutate } from './mutate.js';
+import { getBuildNumber } from './util/getVersionInfo.js';
 
 async function getTemplate() {
-  const template = await readFile('./buildTemplate.js');
+  const template = await readFile('./templates/buildTemplate.js');
   return template.toString();
 }
 
 async function buildHtml(string) {
-  let html = await readFile('./buildTemplate.html');
+  let html = await readFile('./templates/buildTemplate.html');
   html = html.toString().replace(/\{\{\{LOADER\}\}\}/, string.toString());
-
   const data = new Uint8Array(Buffer.from(html));
-  const promise = writeFile('./TestBuild.html', data);
-
-  await promise;
+  const promise = await writeFile('./TestBuild.html', data);
   console.log('HTML Build Operation Complete\r\nResult: %s\r\n[End of Result]', html);
-
   return html;
 }
 
-
-async function fillTemplate(mainString) {
-  const nonDestructiveAssign = await readFile('../src/util/nonDestructiveAssign.js');
-  mainString = mainString.replaceAll(/\{\{\{function_nonDestructiveAssign\}\}\}/g, nonDestructiveAssign.toString());
-
-  const uuidGenerator = await readFile('../src/uuidGenerator.js');
-  mainString = mainString.replace(/\{\{\{function_uuidGenerator\}\}\}/, uuidGenerator.toString());
-
-  const navLinkInjector = await readFile('../src/navLinkInjector.js');
-  mainString = mainString.replace(/\{\{\{function_navLinkInjector\}\}\}/, navLinkInjector.toString());
-
-  const updateNotifier = await readFile('../src/updateNotifier.js');
-  mainString = mainString.replace(/\{\{\{function_updateNotifier\}\}\}/, updateNotifier);
-
-  const buildGlobalObject = await readFile('../src/buildGlobalObject.js');
-  mainString = mainString.replace(/\{\{\{function_buildGlobalObject\}\}\}/, buildGlobalObject.toString());
-
-  const FlashCard = await readFile('../src/FlashCard.js');
-  mainString = mainString.replace(/\{\{\{class_FlashCard\}\}\}/, FlashCard.toString());
-
-  const FlashCardSet = await readFile('../src/FlashCardSet.js');
-  mainString = mainString.replace(/\{\{\{class_FlashCardSet\}\}\}/, FlashCardSet.toString());
-
-  const styles = await readFile('../src/styles.css');
-  mainString = mainString.replace(/\{\{\{css_styles\}\}\}/, styles.toString());
-
-  mainString = mutate(mainString);
-
-  const data = new Uint8Array(Buffer.from(mainString));
-  const promise = writeFile('./TestBuild.js', data);
-
-  await promise;
-  console.log('JavaScript Build Operation Complete\r\nResult: %s\r\n[End of Result]', mainString);
-
-  return mainString;
+async function mutateResult(string) {
+  return mutate(string);
 }
 
-getTemplate()
-  .then((template) => fillTemplate(template))
-  .then((string) => buildHtml(string));
+async function fillTemplate(string, contentFilePath, regexMatch) {
+  const fillContents = await readFile(contentFilePath);
+  return string.replaceAll(regexMatch, fillContents.toString());
+}
+
+async function buildJavascript(string, buildArray) {
+  const fileCount = buildArray.length;
+  for (let index = 0; index < fileCount; index++) {
+    const [path, regex] = buildArray[index];
+    string = await fillTemplate(string, path, regex);
+  }
+  return string;
+}
+
+async function bumpBuildNumber() {
+  const fillContents = await readFile('./util/BUILD');
+  const build = parseInt(fillContents.toString(), 10) + 1;
+  const buildData = new Uint8Array(Buffer.from(build.toString()));
+  await writeFile('./util/BUILD', buildData);
+  const fullBuildNumber = getBuildNumber();
+  const data = new Uint8Array(Buffer.from(fullBuildNumber));
+  await writeFile('./BUILD_NUMBER', data);
+  return;
+}
 
 const replaceArray = [
-  ['../src/util/nonDestructiveAssign.js', /\{\{\{function_nonDestructiveAssign\}\}\}/],
-  ['../src/uuidGenerator.js', /\{\{\{function_uuidGenerator\}\}\}/],
-  ['../src/navLinkInjector.js', /\{\{\{function_navLinkInjector\}\}\}/],
-  ['../src/updateNotifier.js', /\{\{\{function_updateNotifier\}\}\}/],
-  ['../src/buildGlobalObject.js', /\{\{\{function_buildGlobalObject\}\}\}/],
-  ['../src/FlashCard.js', /\{\{\{class_FlashCard\}\}\}/],
-  ['../src/FlashCardSet.js', /\{\{\{class_FlashCardSet\}\}\}/],
-  ['../src/styles.css', /\{\{\{css_styles\}\}\}/]
+  ['../src/util/nonDestructiveAssign.js', /\{\{\{function_nonDestructiveAssign\}\}\}/g],
+  ['../src/uuidGenerator.js', /\{\{\{function_uuidGenerator\}\}\}/g],
+  ['../src/navLinkInjector.js', /\{\{\{function_navLinkInjector\}\}\}/g],
+  ['../src/updateNotifier.js', /\{\{\{function_updateNotifier\}\}\}/g],
+  ['../src/buildGlobalObject.js', /\{\{\{function_buildGlobalObject\}\}\}/g],
+  ['../src/FlashCard.js', /\{\{\{class_FlashCard\}\}\}/g],
+  ['../src/FlashCardSet.js', /\{\{\{class_FlashCardSet\}\}\}/g],
+  ['../src/styles.css', /\{\{\{css_styles\}\}\}/g],
+  ['./BUILD_NUMBER', /\{\{\{id_buildNumber\}\}\}/g]
 ];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+bumpBuildNumber()
+  .then(() => getTemplate())
+  .then((javaScriptTemplate) => buildJavascript(javaScriptTemplate, replaceArray))
+  .then((parsedTemplate) => mutateResult(parsedTemplate))
+  .then((mutatedResult) => buildHtml(mutatedResult));
